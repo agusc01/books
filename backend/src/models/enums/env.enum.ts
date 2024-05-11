ec3f786d28932dfd8f52c268dd35623b47eab4e0
agus c <33077305+agusc01@users.noreply.github.com>
2024-05-11 12:47:35 -0300

    [FEAT] dotenv

---
+++ backend/src/models/enums/env.enum.ts
@@ -0,0 +1,10 @@
+export enum Env {
+    APP_PORT = 'APP_PORT',
+    APP_HOST = 'APP_HOST',
+    DB_PORT = 'DB_PORT',
+    DB_HOST = 'DB_HOST',
+    DB_USER = 'DB_USER',
+    DB_PASS = 'DB_PASS',
+    DB_NAME = 'DB_NAME',
+    DB_TABLE_BOOKS = 'DB_TABLE_BOOKS',
+}
