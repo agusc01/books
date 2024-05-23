47240b6c23a0884076068d35f9b076cb09ef1747
agus c <33077305+agusc01@users.noreply.github.com>
2024-05-22 22:17:28 -0300

    [FIX] actualizar ruta al modificar con ERROR

--- backend/src/utils/renderTo.util.ts
+++ backend/src/utils/renderTo.util.ts
@@ -1,16 +1,32 @@
 import { Request, Response } from "express";
-import { createBookGET, listBooksGET } from "../controllers/book.controller";
+import { createBookGET, listBooksGET, updateBookGET } from "../controllers/book.controller";
 import { errorGet } from "../controllers/error.controller";
 import { ValidRouter } from "../models/types/valid-router.type";
 import { setNewHref } from "./scripts.util";
 
 export const renderTo = (req: Request, res: Response, validRouter: ValidRouter): void => {
+
+    // * Sacar parametros
+    validRouter = validRouter.split("?")[0] as ValidRouter;
+
+    // * Sacar numeros (posibles ID)
+    /*
+    Explicación:
+        \/\d+$:
+        \/ coincide con el carácter /.
+        \d+ coincide con uno o más dígitos.
+        $ asegura que la coincidencia esté al final de la cadena. 
+    */
+    validRouter = validRouter.replace(/\/\d+$/, '') as ValidRouter;
+
     switch (validRouter) {
         case '/libro/crear':
             setNewHref(res, validRouter);
             return createBookGET(req, res);
+        case '/libro/modificar':
+            setNewHref(res, validRouter);
+            return updateBookGET(req, res);
         case '/libro/listar':
-            console.log({ validRouter });
             setNewHref(res, validRouter);
             return listBooksGET(req, res);
         default:
